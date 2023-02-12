d01d8600f77af83be146673848055b0cb48e3b5f
Sergey Grigorev <grigoscool@mail.ru>
2023-02-12 11:21:36 +0300

    add BonusCard model

--- bonus_card/models.py
+++ bonus_card/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BonusCard(models.Model):
+    serial_num = models.PositiveIntegerField()
+    card_num = models.PositiveIntegerField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_end = models.DateTimeField()
+    date_use = models.DateTimeField()
+    balance = models.IntegerField()
+    card_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Card № {self.card_num}'
