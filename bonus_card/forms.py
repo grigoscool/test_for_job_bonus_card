a4a7762c122e638f5ab3ec60aa790244477e0135
Sergey Grigorev <grigoscool@mail.ru>
2023-02-21 20:53:24 +0300

    add css

--- bonus_card/forms.py
+++ bonus_card/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
+from django import forms
+
 from .models import BonusCard
 
 
 class BonusCardForm(ModelForm):
+    serial_num = forms.IntegerField(help_text='только 4х значные')
     class Meta:
         model = BonusCard
         fields = ['serial_num', 'card_num', 'date_end']
+
+
