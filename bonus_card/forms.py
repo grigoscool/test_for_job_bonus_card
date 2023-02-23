877aa675295b116c2453c22549cb57b3c1fd64cb
Sergey Grigorev <grigoscool@mail.ru>
2023-02-23 15:57:06 +0300

    add style

--- bonus_card/forms.py
+++ bonus_card/forms.py
@@ -1,16 +1,27 @@
 from django.core.exceptions import ValidationError
-from django.forms import ModelForm
 from django import forms
 
 from .models import BonusCard
 
 
-class BonusCardForm(ModelForm):
-    serial_num = forms.IntegerField(help_text='только 4х значные')
-
+class BonusCardForm(forms.ModelForm):
+    """
+    Форма для создания бонусной карты.
+    """
     class Meta:
         model = BonusCard
         fields = ['serial_num', 'card_num', 'date_end']
+        widgets = {
+            'serial_num': forms.TextInput(attrs={'class': 'form-control'}),
+            'card_num': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_end': forms.TextInput(attrs={'type': 'datetime-local',
+                                               'class': 'form-control'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(BonusCardForm, self).__init__(*args, **kwargs)
+        self.fields['serial_num'].help_text = 'только 4х значные'
+            
 
     def clean_card_num(self):
         # проверка на номер карты больше 100
@@ -20,6 +31,7 @@ class BonusCardForm(ModelForm):
         return card_num
 
     def clean(self):
+        # проверка что серийник и номер карты не совпадают
         serial_num = self.cleaned_data.get('serial_num')
         card_num = self.cleaned_data.get('card_num')
         if card_num == serial_num:
